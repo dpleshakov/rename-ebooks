140a444a6996008208d89d8bb9cf4ca46bcbac0b
Dmitry Pleshakov <dpleshakov@gmail.com>
2025-07-12 20:02:53 +0300

    Move tests to a special folder

--- tests/tests_rename_ebooks.py
+++ tests/tests_rename_ebooks.py
@@ -4,7 +4,7 @@ import pytest
 from unittest.mock import patch, MagicMock
 
 
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import rename_ebooks
 
 
